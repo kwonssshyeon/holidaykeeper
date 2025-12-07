972f0eb0677dec8a58edcb27e900069d44acaf5a
kwonssshyeon <kwonsh4396@gmail.com>
2025-12-07 19:21:46 +0900

    feat: 기본 API 응답 포맷 정의

---
+++ src/main/java/com/planitsquare/holidaykeeper/global/response/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.planitsquare.holidaykeeper.global.response;
+
+import lombok.Getter;
+
+@Getter
+public record ApiResponse<T>(
+        int status,
+        String message,
+        T data
+) {
+    public static <T> ApiResponse<T> ok(T data) {
+        return ApiResponse.of(CustomCode.OK, data);
+    }
+
+    public static <T> ApiResponse<T> error(CustomCode code) {
+        return new ApiResponse<>(code.getStatus(), code.getMessage(), null);
+    }
+
+    public static <T> ApiResponse<T> of(CustomCode code, T data) {
+        return new ApiResponse<>(code.getStatus(), code.getMessage(), data);
+    }
+
+    public static <T> ApiResponse<T> of(CustomCode code) {
+        return new ApiResponse<>(code.getStatus(), code.getMessage(), null);
+    }
+}
