07d6928c5ce8b785bb777def1f52ba3be8a746d9
kwonssshyeon <kwonsh4396@gmail.com>
2025-12-07 14:51:25 +0900

    fix: 공휴일 타입, 지역 테이블 JPA 복합키 정의 방법 수정

--- src/main/java/com/planitsquare/holidaykeeper/domain/HolidayType.java
+++ src/main/java/com/planitsquare/holidaykeeper/domain/HolidayType.java
@@ -16,6 +16,11 @@ public class HolidayType {
     @EmbeddedId
     private HolidayTypeId id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("holidayId") // HolidayCountyId 안의 holidayId와 매핑
+    @JoinColumn(name = "holiday_id", nullable = false)
+    private Holiday holiday;
+
     @Getter
     @Embeddable
     @NoArgsConstructor
@@ -24,8 +29,14 @@ public class HolidayType {
         @Column(name = "type_name", nullable = false, length = 20)
         private String typeName;
 
-        @ManyToOne(fetch = FetchType.LAZY)
         @JoinColumn(name = "holiday_id", nullable = false)
-        private Holiday holiday;
+        private Long holidayId;
+    }
+
+    HolidayType(Holiday holiday, String typeName) {
+        this.holiday = holiday;
+        this.id = new HolidayTypeId();
+        this.id.holidayId = holiday.getId();
+        this.id.typeName = typeName;
     }
 }
