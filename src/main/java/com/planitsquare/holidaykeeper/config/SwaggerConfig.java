0e3179a0ab1ab023a6cced78494f6cdbfec51b74
kwonssshyeon <kwonsh4396@gmail.com>
2025-12-07 21:19:01 +0900

    fix: swagger 버전 변경

--- src/main/java/com/planitsquare/holidaykeeper/config/SwaggerConfig.java
+++ src/main/java/com/planitsquare/holidaykeeper/config/SwaggerConfig.java
@@ -1,24 +1,25 @@
 package com.planitsquare.holidaykeeper.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
-
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
-    }
-    private Info apiInfo() {
-        return new Info()
-                .title("Holiday Keeper API Documentation")
-                .description("Holiday Keeper API 명세입니다.")
-                .version("1.0.0");
+                .info(new Info()
+                        .title("StarLog API")
+                        .version("v1")
+                        .description("StarLog 서비스 API 문서"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local server")
+                ));
     }
 }
+
